fea7bd536e54131eaa67d571b92c8c749bdaa1a3
NatalliaKhaladzinskaya <32439590+NatalliaKhaladzinskaya@users.noreply.github.com>
2019-03-19 18:16:35 +0300

    Initial commit

---
+++ src/task3209/View.java
@@ -0,0 +1,163 @@
+package task3209;
+
+
+import task3209.listeners.FrameListener;
+import task3209.listeners.TabbedPaneChangeListener;
+import task3209.listeners.UndoListener;
+
+import javax.swing.*;
+import javax.swing.undo.UndoManager;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class View extends JFrame implements ActionListener {
+    private Controller controller;
+    private JTabbedPane tabbedPane = new JTabbedPane();
+    private JTextPane htmlTextPane = new JTextPane();
+    private JEditorPane plainTextPane = new JEditorPane();
+    private UndoManager undoManager = new UndoManager();
+    private UndoListener undoListener = new UndoListener(undoManager);
+
+    public View() {
+        try {
+            String systemLookAndFeelClassName = UIManager.getSystemLookAndFeelClassName();
+            // устанавливаем LookAndFeel
+            UIManager.setLookAndFeel(systemLookAndFeelClassName);
+        } catch (Exception e) {
+            ExceptionHandler.log(e);
+        }
+    }
+
+    public Controller getController() {
+        return controller;
+    }
+
+    public void setController(Controller controller) {
+        this.controller = controller;
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        switch (e.getActionCommand()) {
+            case "Новый":
+                controller.createNewDocument();
+                break;
+            case "Открыть":
+                controller.openDocument();
+                break;
+            case "Сохранить":
+                controller.saveDocument();
+                break;
+            case "Сохранить как...":
+                controller.saveDocumentAs();
+                break;
+            case "Выход":
+                controller.exit();
+                break;
+            case "О программе":
+                showAbout();
+                break;
+        }
+
+    }
+
+    public void selectedTabChanged() {
+        if (tabbedPane.getSelectedIndex() == 0) controller.setPlainText(plainTextPane.getText());
+        else if (tabbedPane.getSelectedIndex() == 1) plainTextPane.setText(controller.getPlainText());
+        resetUndo();
+    }
+
+    public void init() {
+        initGui();
+        FrameListener frameListener = new FrameListener(this);
+        addWindowListener(frameListener);
+        setVisible(true);
+    }
+
+    public void exit() {
+        controller.exit();
+    }
+
+    public void initMenuBar() {
+        JMenuBar jMenuBar = new JMenuBar();
+        MenuHelper.initFileMenu(this, jMenuBar);
+        MenuHelper.initEditMenu(this, jMenuBar);
+        MenuHelper.initStyleMenu(this, jMenuBar);
+        MenuHelper.initAlignMenu(this, jMenuBar);
+        MenuHelper.initColorMenu(this, jMenuBar);
+        MenuHelper.initFontMenu(this, jMenuBar);
+        MenuHelper.initHelpMenu(this, jMenuBar);
+        getContentPane().add(jMenuBar, BorderLayout.NORTH);
+    }
+
+    public void initEditor() {
+        htmlTextPane.setContentType("text/html");
+        JScrollPane jScrollPaneHtml = new JScrollPane(htmlTextPane);
+        tabbedPane.add("HTML", jScrollPaneHtml);
+        JScrollPane jScrollPaneText = new JScrollPane(plainTextPane);
+        tabbedPane.add("Текст", jScrollPaneText);
+        tabbedPane.setPreferredSize(new Dimension(300, 300));
+        TabbedPaneChangeListener listener = new TabbedPaneChangeListener(this);
+        tabbedPane.addChangeListener(listener);
+        getContentPane().add(tabbedPane, BorderLayout.CENTER);
+    }
+
+    public void initGui() {
+        initMenuBar();
+        initEditor();
+        pack();
+    }
+
+    public boolean canUndo() {
+        return undoManager.canUndo();
+    }
+
+    public boolean canRedo() {
+        return undoManager.canRedo();
+    }
+
+    public UndoListener getUndoListener() {
+        return undoListener;
+    }
+
+    public void undo() {
+        try {
+            undoManager.undo();
+        } catch (Exception e) {
+            ExceptionHandler.log(e);
+        }
+    }
+
+    public void redo() {
+        try {
+            undoManager.redo();
+        } catch (Exception e) {
+            ExceptionHandler.log(e);
+        }
+    }
+
+    public void resetUndo() {
+        undoManager.discardAllEdits();
+    }
+
+    public boolean isHtmlTabSelected() {
+        if (tabbedPane.getSelectedIndex() == 0) return true;
+        else return false;
+    }
+
+    public void selectHtmlTab() {
+        tabbedPane.setSelectedIndex(0);
+        resetUndo();
+    }
+
+    public void update() {
+        htmlTextPane.setDocument(controller.getDocument());
+    }
+
+    public void showAbout() {
+        JOptionPane.showMessageDialog(getContentPane(), "Version 1.0", "About", JOptionPane.INFORMATION_MESSAGE);
+    }
+
+
+}
